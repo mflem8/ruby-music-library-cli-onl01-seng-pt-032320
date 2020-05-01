149c4d167ce0207fbb13ceeac1207d7f6fec175f
Matt Fleming <matthewfleming8@gmail.com>
2020-05-01 02:30:53 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -9,33 +9,35 @@ class MusicLibraryController
   def call
     input = ""
 
-    puts "Welcome to your music library!"
-    puts "To list all of your songs, enter 'list songs'."
-    puts "To list all of the artists in your library, enter 'list artists'."
-    puts "To list all of the genres in your library, enter 'list genres'."
-    puts "To list all of the songs by a particular artist, enter 'list artist'."
-    puts "To list all of the songs of a particular genre, enter 'list genre'."
-    puts "To play a song, enter 'play song'."
-    puts "To quit, type 'exit'."
-    puts "What would you like to do?"
+    while input != "exit"
+      puts "Welcome to your music library!"
+      puts "To list all of your songs, enter 'list songs'."
+      puts "To list all of the artists in your library, enter 'list artists'."
+      puts "To list all of the genres in your library, enter 'list genres'."
+      puts "To list all of the songs by a particular artist, enter 'list artist'."
+      puts "To list all of the songs of a particular genre, enter 'list genre'."
+      puts "To play a song, enter 'play song'."
+      puts "To quit, type 'exit'."
+      puts "What would you like to do?"
 
-    input = gets.chomp
+      input = gets.chomp
 
-   case input
-   when "list songs"
-     list_songs
-   when "list artists"
-     list_artists
-   when "list genres"
-     list_genres
-   when "list artist"
-     list_songs_by_artist
-   when "list genre"
-     list_songs_by_genre
-   when "play song"
-     play_song
-   end
- end
+      case input
+      when "list songs"
+        list_songs
+      when "list artists"
+        list_artists
+      when "list genres"
+        list_genres
+      when "list artist"
+        list_songs_by_artist
+      when "list genre"
+        list_songs_by_genre
+      when "play song"
+        play_song
+      end
+    end
+  end
 
  def list_songs_by_artist
    puts "Please enter the name of an artist:"
@@ -52,4 +54,42 @@ class MusicLibraryController
    Artist.all.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |a, i|
      puts "#{i}. #{a.name}"
    end
+ end
+
+ def list_genres
+   Genre.all.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |g, i|
+     puts "#{i}. #{g.name}"
+   end
+ end
+
+ def list_songs_by_artist
+   puts "Please enter the name of the artist:"
+   input = gets.chomp
+   if artist = Artist.find_by_name(input)
+     artist.songs.sort{ |a, b| a.name <=> b.name}.each.with_index(1) do |s, i|
+       puts "#{i}. #{s.name} - #{s.genre.name}"
+     end
+   end
+ end
+
+ def list_songs_by_genre
+   puts "Please enter the name of a genre:"
+   input = gets.chomp
+   if genre = Genre.find_by_name(input)
+     genre.songs.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |s, i|
+       puts "#{i}. #{s.artist.name} - #{s.name}"
+     end
+   end
+ end
+
+ def play_song
+   puts "Which song number would you like to play?"
+   input = gets.chomp.to_i
+   if (1..Song.all.length).include?(input)
+     song = Song.all.sort{ |a, b| a.name <=> b.name }[input - 1]
+   end
+
+   puts "Playing #{song.name} by #{song.artist.name}" if song
+ end
+
 end
